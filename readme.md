684c6748c5b2499132315f8fa0014ee0b631248a
burntfalafel <wadhwae@tcd.ie>
2021-08-29 09:18:07 +0100

    [working] added a timing result such that compiler optimisation isn't doubted

--- readme.md
+++ readme.md
@@ -1,11 +1,14 @@
 # Vectors in C
 A lite-weight preprocessor macro only library which replicates `std::vector` with minimum overhead!. Each of the tests can be compiled using 
 ```
-clang -g3 -Wall test/st_dyn_timed_tests.c -Iinclude/ && ./a.out
+clang -g3 -Wall test/main.c -Iinclude/ && ./a.out
 ```
 
 # Why use it?
 Using inline funcions may dirty the code a bit after the preprocessing stage, but it is worth it! 
+
+
+Timed output for a `char` input (`st_dyn_timed_tests.c`)
 ```
 Dynamic memory allocation uses this address: 0x562bc1a7e2a0
 A dynamic memory allocation which uses linked lists use time   0.00281
@@ -15,4 +18,15 @@ A static allocation which uses arrays use time   0.00001
 
 Dynamic memory vector allocation uses this address: 0x562bc1a863b0
 Dynamic allocation which uses vectors use time   0.00002
+```
+
+Timed output for a `int` input (`st_int_timed_tests.c`)
+```
+A dynamic memory allocation which uses linked lists use time   0.00291
+
+Static memory allocation uses this address: 0x7ffc4306e390
+A static allocation which uses arrays use time   0.00001
+
+Dynamic memory vector allocation uses this address: 0x5561c20a23b0
+Dynamic allocation which uses vectors use time   0.00003
 ```
---
+++ test/st_int_timed_tests.c
@@ -0,0 +1,96 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+#include "st_vector.h"
+
+/* should pertain the limits offered by the stack memory count
+ * can be seen on your system through `ulimit -s`
+ */
+#define SAMPLE_COUNT (10U * 100U)
+
+
+typedef struct Node {
+	int data;
+	struct Node* next;
+} Node;
+Node* addList(Node* ptr, char data)
+{
+	if(ptr==NULL)
+	{
+		ptr = malloc(sizeof(Node));
+		ptr->data = data;
+		ptr->next = NULL;
+		return ptr;
+	}
+	Node *start = ptr;
+	while(ptr->next!=NULL)
+		ptr=ptr->next;
+	Node* new = malloc(sizeof(Node));
+	new->data = data;
+	ptr->next = new;
+	new->next = NULL;
+	return start;
+}
+int printList(Node* ptr)
+{
+	while(ptr->next!=NULL)
+	{
+		printf("%c\n",ptr->data);
+		ptr = ptr->next;
+	}
+	printf("%c\n", ptr->data);
+	return 0;
+}
+int deleteList(Node *ptr)
+{
+	while(ptr->next!=NULL)
+	{
+		Node *next = ptr->next;
+		free(ptr);
+		ptr = next;
+	}
+	free(ptr);
+	return 0;
+}
+
+int main()
+{
+  /*  A benchmark of a dynamic memory allocation
+   *  uses heap address. 
+   */
+	Node* ptr = NULL;
+	float startTime = (float)clock()/CLOCKS_PER_SEC;
+	for(int i=0;i<SAMPLE_COUNT;i++)
+		ptr = addList(ptr, i);
+	float endTime = (float)clock()/CLOCKS_PER_SEC;
+	float timeElapsed = endTime - startTime;
+  printf("Dynamic memory allocation uses this address: %p",ptr);
+	printf("\nA dynamic memory allocation which uses linked lists use time %9.5f\n", timeElapsed);
+	deleteList(ptr);
+  /* A benchmark of a static memory allocation
+   * uses stack address. 
+   */
+	startTime = (float)clock()/CLOCKS_PER_SEC;
+	int array[SAMPLE_COUNT];
+	for(int i=0; i<SAMPLE_COUNT; i++)
+		array[i] = i;
+	endTime = (float)clock()/CLOCKS_PER_SEC;
+	timeElapsed = endTime - startTime;
+  printf("\nStatic memory allocation uses this address: %p",array);
+	printf("\nA static allocation which uses arrays use time %9.5f\n", timeElapsed);
+  /* A benchmark of a static memory allocation
+   * uses stack address. 
+   */
+	startTime = (float)clock()/CLOCKS_PER_SEC;
+  st_vector(int) vector;
+  st_init(vector);
+	for(int i=0; i<SAMPLE_COUNT; i++)
+    st_push(vector, i);
+	endTime = (float)clock()/CLOCKS_PER_SEC;
+	timeElapsed = endTime - startTime;
+  printf("\nDynamic memory vector allocation uses this address: %p",vector.data);
+	printf("\nDynamic allocation which uses vectors use time %9.5f\n", timeElapsed);
+  st_free(vector);
+	return 0;
+}
